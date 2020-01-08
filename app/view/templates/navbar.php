32adb28e07bec5e808841b14ed24e2947852eebf
ratoz <herlandro.1999@students.amikom.ac.id>
2020-01-08 21:13:25 +0700

    updata

--- app/view/templates/navbar.php
+++ app/view/templates/navbar.php
@@ -28,7 +28,7 @@
               }
               else{
               $foto = PATHFOTO;}
-            
+
                 ?>
               <?php if (!file_exists($foto . $data['profil']['profilfoto'])) {
                 if ($data['profil']['gender'] === "Laki-Laki") { ?>
@@ -95,11 +95,7 @@
           </a>
         </li>
         <li class="nav-item">
-<<<<<<< HEAD
-          <a class="nav-link " href="<?= BASEURL; ?>Profile">
-=======
           <a class="nav-link <?php if ($data['page'] === 'profile') echo "active"; ?>" href="<?= BASEURL; ?>Profile">
->>>>>>> 2c1659b1c2dd8fbe8e8521cea835974a735d1978
             <i class="ni ni-single-02 text-yellow"></i> Profil Pengguna
           </a>
         </li>
