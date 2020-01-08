d33c6974e9cb2080b391008d3b8a6dcad5f04728
ratoz <herlandro.1999@students.amikom.ac.id>
2020-01-09 06:58:00 +0700

    valid update caripartner (ando)

--- app/view/caripartner/valid.php
+++ app/view/caripartner/valid.php
@@ -17,7 +17,7 @@
             <div class="row align-items-center">
               <div class="col-12">
                 <div class="text-center">
-                  <h3 class="mb-0">Data Nilai Kamu</h3>
+                  <h3 class="mb-0">Data Pencarian Partner</h3>
                 </div>
               </div>
             </div>
@@ -25,13 +25,13 @@
           <div class="card-body">
             <form method="post" action="#">
               <div class="text-center">
-                <h6 class="heading-small text-muted mb-4">Masukkan Data Nilai Kamu</h6>
+                <h6 class="heading-small text-muted mb-4">Masukkan Data Pencarian</h6>
               </div>
               <div class="pl">
                 <div class="row">
                   <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label">Mata Pelajaran</label>
+                      <label class="form-control-label">Mata Pelajaran terfavorit</label>
                       <select class="form-control form-control-alternative" name="mapel_tinggi">
                         <option value="MP001">Pendidikan Pancasila dan Kewarganegaraan</option>
                         <option value="MP002">Matematika</option>
@@ -59,27 +59,9 @@
                   </div>
                 </div>
                 <div class="row">
-                  <div class="col-lg-6">
+                <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label">Nilai Tinggi</label>
-                      <input type="text" class="form-control form-control-alternative" name="nilai_tinggi">
-                    </div>
-                  </div>
-                  <div class="col-lg-6">
-                    <div class="form-group">
-                      <label class="form-control-label">Filter Data Kelas</label>
-                      <select class="form-control form-control-alternative" name="tingkat_kelas">
-                        <option value="1">Kelas 1</option>
-                        <option value="2">Kelas 2</option>
-                        <option value="3">Kelas 3</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-lg-6">
-                    <div class="form-group">
-                      <label class="form-control-label">Mata Pelajaran (Partner)</label>
+                      <label class="form-control-label">Mata Pelajaran yang ingin dipelajari</label>
                       <select class="form-control form-control-alternative" name="mapel_rendah">
                         <option value="MP001">Pendidikan Pancasila dan Kewarganegaraan</option>
                         <option value="MP002">Matematika</option>
@@ -95,14 +77,19 @@
                       </select>
                     </div>
                   </div>
-                </div>
-                <div class="row">
                   <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label">Nilai Rendah</label>
-                      <input type="text" class="form-control form-control-alternative" name="nilai_rendah">
+                      <label class="form-control-label">Filter Data Kelas</label>
+                      <select class="form-control form-control-alternative" name="tingkat_kelas">
+                        <option value="1">Kelas 1</option>
+                        <option value="2">Kelas 2</option>
+                        <option value="3">Kelas 3</option>
+                      </select>
                     </div>
                   </div>
+                </div>
+                <div class="row">
+
                 </div>
                 <div class="text-center">
                   <button type="submit" class="btn btn-primary my-4" name="submit">Cari Partner</button>
