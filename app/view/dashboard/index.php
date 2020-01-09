11876eb18bcb057a238fb78ca33b61c7da495be7
akbarHm <harimukti789@gmail.com>
2020-01-09 10:07:47 +0700

    update dashboard

--- app/view/dashboard/index.php
+++ app/view/dashboard/index.php
@@ -1,5 +1,31 @@
 <div class="header bg-gradient-success pb-4 pt-5 pt-md-6">
 
+<!-- 
+<div class="base">
+  <div class="holder"></div>
+  <div class="thread">
+    <div class="pendulum" style="color: white">Circle Of Science</div>
+  </div>
+  <div class="shadow"></div>
+</div>
+<br>
+-->
+<div class="container-fluid mb-4">
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" style="height: 250px; object-fit: cover" src="img/slide1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" style="height: 250px; object-fit: cover" src="img/slide2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 " style="height: 250px; object-fit: cover" src="img/slide3.jpg" alt="Third slide">
+    </div>
+  </div>
+</div>
+</div>
+
   <div class="container-fluid">
     <div class="form-search-wrap bg-default p-3 rounded" data-aos="fade-up" data-aos-delay="200">
 
